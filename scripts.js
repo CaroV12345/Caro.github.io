05755768ba54a36a84ff232ac0c52cdc5a18652f
CaroV12345 <55685696+CaroV12345@users.noreply.github.com>
2019-10-08 14:00:08 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function muestraFraseFecha() {
+    var saludo = "Hola Caro ";
+    document.getElementById('fecha').innerHTML = saludo + Date();
+  }
+
+function toCelsius (fahrenheit){
+      return (5/9) * (fahrenheit-32);
+  }
+
+  
